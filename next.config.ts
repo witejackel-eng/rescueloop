d6134da1167b9fe19f56e3d8e1c576fbf2611d48
witejackel-eng <291486779+witejackel-eng@users.noreply.github.com>
2026-08-07 05:56:10 +0000

    f74c0c0d-1315-4413-8a52-feacd64a08e3

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   reactStrictMode: false,
+  allowedDevOrigins: [
+    "preview-chat-249bca23-0171-4d66-a68e-66b08d8fdbd3.space-z.ai",
+  ],
 };
 
 export default nextConfig;
